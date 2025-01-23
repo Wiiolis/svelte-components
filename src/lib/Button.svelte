07995630d91ab6d4bfa671505d14915eeae85141
Kristina Maximova <wiiolis@verotel.cz>
2025-01-23 14:01:06 +0100

    Create Button component that accepts size and type
    Signed-off-by: Kristina Maximova <wiiolis@verotel.cz>

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,7 +1,62 @@
 <svelte:options customElement="swc-button"/>
 
-<button>{innerText}</button>
+<button class={getClasses()}>{@html innerText}</button>
 
-<script>
+<script lang="ts">
     export let innerText = ''
+    export let size = 'large' | 'medium' | 'small';
+    export let type = 'ghost' | 'link' | 'normal';
+
+
+    const getClasses = () => {
+        let classes = ''
+
+        if (size) {
+            classes = classes.concat(" ", size);
+        }
+
+        if (type) {
+            classes = classes.concat(" ", type);
+        }
+
+        return classes;
+    };
 </script>
+
+<style lang="scss">
+    button {
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        font-size: 16px;
+        cursor: pointer;
+        font-weight: 500;
+    }
+
+    .link {
+        color: #1b95e0;
+        background-color: transparent;
+        border: none;
+
+    }
+
+    .ghost {
+        color: #1b95e0;
+        background-color: transparent;
+        border: none;
+    }
+
+    .normal {
+        background-color: #2f8eec;
+        color: white;
+        font-weight: 600;
+        border-radius: 7px;
+        background-color: #2186eb;
+    }
+
+    .large {
+        padding: 30px;
+    }
+
+    .small {
+        padding: 10px;
+    }
+</style>
